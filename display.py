d0fa59d5b317ef3fd25bbffde516b33562a41755
RifkyJustian <87746043+RifkyJustian@users.noreply.github.com>
2026-05-08 11:25:09 +0700

    Create display.py

---
+++ display.py
@@ -0,0 +1,7 @@
+def welcome():
+    print("================================")
+    print("🎮 GUESS THE NUMBER GAME")
+    print("================================")
+
+def show_result(message):
+    print(message)
